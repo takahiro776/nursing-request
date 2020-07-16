29fe3885e00b7e387f39b5b3011df28d6cb416fe
takahiro776 <homareendo@gmail.com>
2020-07-17 07:42:07 +0900

    informationsテーブルの作成とモデルにバリデーション、アソシエーションの記述

---
+++ app/models/infomation.rb
@@ -0,0 +1,7 @@
+class Infomation < ApplicationRecord
+  has_many :images
+  has_many :informations_categorys
+  has_many :categorys, through: :informations_categorys
+
+  validates :content, presence: true
+end
