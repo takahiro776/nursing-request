26e81bf45435a7698ec0629e9dac68ccb8cd3830
takahiro776 <homareendo@gmail.com>
2020-08-09 19:49:21 +0900

    seeds.rbにカテゴリの初期データ記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+category_ary = ["重要","医療","業務連絡","気付き"]
+
+category_ary.each do |category|
+  Category.create(name: category)
+end
