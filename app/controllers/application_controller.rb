7a21190bb2dcdf0b1d61accfb1082dcfc00cd5ab
takahiro776 <homareendo@gmail.com>
2020-08-15 17:59:10 +0900

    basec認証を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic][:BASIC_AUTH_USER] && password == Rails.application.credentials[:basic][:BASIC_AUTH_PASSWORD]
+    end
   end
 end
