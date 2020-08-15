28e2cf7b2c8a40a7857f737e26bd1c87abdd2509
takahiro776 <homareendo@gmail.com>
2020-08-15 17:14:28 +0900

    パスワードのバリデーション変更とログインボタンを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :name,presence: true
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
